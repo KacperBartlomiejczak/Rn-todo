b322c2e2e68c2c5d49fdfaf3f61c9ea03b2d2952
Kacperbartlomiejczak <bartlomiejczakkapi@gmail.com>
2025-06-03 10:10:51 +0200

    Added Modal

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
-import { Text, View, FlatList } from "react-native";
+import { Text, View, FlatList, Modal, Pressable, TextInput } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import DateTimePicker from 'react-native-ui-datepicker';
 
 import { allTasks } from "@/data/allTask";
 import { getCategoryColor } from "@/utils/getCategoryColor";
@@ -8,12 +9,21 @@ import HeaderView from "@/components/headerView";
 import AddTaskBtn from "@/components/addTaskBtn";
 import TaskCard from "@/components/taskCard";
 
+
+import {useState} from 'react'
+
 export default function Index() {
+
+  const [isVisible, setIsVisible] = useState(false)
+  const handleVisible = () => {
+    setIsVisible((lastState) => !lastState)
+  }
+
   return (
     <View>
       <SafeAreaView className="relative h-full p-4">
         <HeaderView />
-        <AddTaskBtn />
+        <AddTaskBtn onVisible={handleVisible} />
 
         <FlatList
           data={allTasks}
@@ -28,6 +38,25 @@ export default function Index() {
           keyExtractor={(item) => item.id.toString()}
           className="mt-5"
         />
+
+        <Modal visible={isVisible} animationType="slide" presentationStyle="formSheet">
+           <SafeAreaView className="">
+            <Text>Add Task</Text>
+            <View>
+              <Text>Title</Text>
+              <TextInput placeholder="Task title" />
+            </View>
+            <View>
+              <Text>Date</Text>
+              <DateTimePicker mode="single" ></DateTimePicker>
+            </View>
+
+            <Pressable onPress={handleVisible}>  
+              <Text>Cancel</Text>
+            </Pressable>
+          </SafeAreaView> 
+
+        </Modal>
       </SafeAreaView>
     </View>
   );
--- components/addTaskBtn.tsx
+++ components/addTaskBtn.tsx
@@ -1,24 +1,22 @@
-import { Pressable, View, Text } from "react-native";
+import { Pressable,  Text } from "react-native";
 
 
 
-export default function AddTaskBtn() {
+export default function AddTaskBtn({onVisible} : {onVisible : () => void}) {
   return (
     <Pressable
-      className="w-16 h-16  bg-blue-500 rounded-full flex flex-col items-center justify-center absolute bottom-12 right-8 "
-      style={({ pressed }) => [
-        {
-          backgroundColor: pressed ? "#1d4ed8" : undefined,
-          borderRadius: 24,
-        },
+      className="w-16 h-16 bg-blue-500  rounded-full flex flex-col items-center justify-center absolute bottom-12 right-8 z-50"
+      style={({pressed}) => [
+        {backgroundColor: pressed ? "#1d4ed8" : "blue"},
       ]}
       android_ripple={{
         color: "#1d4ed8",
         borderless: true,
         radius: 24,
       }}
+      onPress={onVisible}
     >
-      <Text className="text-white text-2xl font-semi-bold">+</Text>
+      <Text className="text-white text-2xl font-semibold">+</Text>
     </Pressable>
   );
 }
